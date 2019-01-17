1ecd016dfefd2a49b6a7e0b9cd88a7aad04af97b
Fletcher LaRue <laruefletcher@gmail.com>
2019-01-16 20:35:27 -0800

    finishes main tasks

--- auth-server/index.js
+++ auth-server/index.js
@@ -8,6 +8,13 @@ const options = {
   useNewUrlParser:true,
   useCreateIndex: true,
 };
+
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'database connection error:'));
+db.once('open', function() {
+  console.log(`Database connected at: ${process.env.MONGODB_URI}`);
+});
+
 mongoose.connect(process.env.MONGODB_URI, options);
 
 // Start the web server
